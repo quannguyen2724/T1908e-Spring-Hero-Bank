3536d4ebea5994e97b2cc423560b227bf30efd08
truongph-fpt <truongphth1907039@fpt.edu.vn>
2020-06-12 20:55:35 +0700

    update

--- T1908e-Spring-Hero-Bank/Entity/Transaction.cs
+++ T1908e-Spring-Hero-Bank/Entity/Transaction.cs
@@ -1,7 +1,26 @@
-﻿namespace T1908e_Spring_Hero_Bank.Entity
+﻿using System;
+
+namespace T1908e_Spring_Hero_Bank.Entity
 {
     public class Transaction
     {
-        
+        public string TransactionCode { get; set; }
+        public string SenderAccountNumber { get; set; }
+        public string ReceiverAccountNumber { get; set; }
+        public string Message { get; set; }
+        public double Amount { get; set; }
+        public double Fee { get; set; }
+        public TransactionType Type { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public TransactionStatus Status { get; set; }
+    }
+    public enum TransactionType
+    {
+        Withdraw = 1, Deposit = 2, Tranfer = 3 
+    }
+    public enum TransactionStatus
+    {
+        Pending = 0, Done = 1, Fails = 0
     }
 }
--- T1908e-Spring-Hero-Bank/Program.cs
+++ T1908e-Spring-Hero-Bank/Program.cs
@@ -6,9 +6,7 @@ namespace T1908e_Spring_Hero_Bank
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Git Test");
-            Console.WriteLine("Git Test 2");
-            Console.WriteLine("Git Test 3");
+           
         }
     }
 }
