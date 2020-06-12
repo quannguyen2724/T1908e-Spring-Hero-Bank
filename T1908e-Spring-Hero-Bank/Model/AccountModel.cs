062e1f13b61f6b9896af2773b3e70806ba7349c9
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2020-06-12 23:27:35 +0700

    add check account by username, accountnumber, phone

--- T1908e-Spring-Hero-Bank/Model/AccountModel.cs
+++ T1908e-Spring-Hero-Bank/Model/AccountModel.cs
@@ -1,7 +1,86 @@
-﻿namespace T1908e_Spring_Hero_Bank.Model
+﻿using MySql.Data.MySqlClient;
+using T1908e_Spring_Hero_Bank.Entity;
+using T1908e_Spring_Hero_Bank.Helper;
+
+namespace T1908e_Spring_Hero_Bank.Model
 {
     public class AccountModel
     {
-        
+        private Account _account;
+        public Account GetAccountByUsername(string username)
+        {
+            var cnn = ConnectionHelper.GetConnection();
+            cnn.Open();
+            var cmd = new MySqlCommand($"select * from shbaccount where username = '{username}'", cnn);
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                _account = new Account()
+                {
+                    AccountNumber = reader.GetString("accountNumber"),
+                    Balance = reader.GetDouble("balance"),
+                    Username = reader.GetString("username"),
+                    PasswordHash = reader.GetString("passwordHash"),
+                    Salt = reader.GetString("salt"),
+                    Email = reader.GetString("email"),
+                    Phone = reader.GetString("phone"),
+                    Fullname = reader.GetString("fullname"),
+                    Status = (AccountStatus) reader.GetInt32("status")
+                };
+            }
+            cnn.Close();
+            return _account;
+        }
+
+
+        public Account GetAccountByPhone(string phone)
+        {
+            var cnn = ConnectionHelper.GetConnection();
+            cnn.Open();
+            var cmd = new MySqlCommand($"select * from shbaccount where phone = '{phone}'", cnn);
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                _account = new Account()
+                {
+                    AccountNumber = reader.GetString("accountNumber"),
+                    Balance = reader.GetDouble("balance"),
+                    Username = reader.GetString("username"),
+                    PasswordHash = reader.GetString("passwordHash"),
+                    Salt = reader.GetString("salt"),
+                    Email = reader.GetString("email"),
+                    Phone = reader.GetString("phone"),
+                    Fullname = reader.GetString("fullname"),
+                    Status = (AccountStatus) reader.GetInt32("status")
+                };
+            }
+            cnn.Close();
+            return _account;
+        }
+
+        public Account GetAccountByAccountnumber(string accountNumber)
+        {
+            var cnn = ConnectionHelper.GetConnection();
+            cnn.Open();
+            var cmd = new MySqlCommand($"select * from shbaccount where accountNumber = '{accountNumber}'", cnn);
+            var reader = cmd.ExecuteReader();
+            if (reader.Read())
+            {
+                _account = new Account()
+                {
+                    AccountNumber = reader.GetString("accountNumber"),
+                    Balance = reader.GetDouble("balance"),
+                    Username = reader.GetString("username"),
+                    PasswordHash = reader.GetString("passwordHash"),
+                    Salt = reader.GetString("salt"),
+                    Email = reader.GetString("email"),
+                    Phone = reader.GetString("phone"),
+                    Fullname = reader.GetString("fullname"),
+                    Status = (AccountStatus) reader.GetInt32("status")
+                };
+            }
+            cnn.Close();
+            return _account;
+        }
     }
 }
