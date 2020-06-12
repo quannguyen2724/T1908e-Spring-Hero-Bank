db35d725974d5350a6d709f65de4a0f7e8cadc36
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2020-06-12 21:01:52 +0700

    add helper

---
+++ T1908e-Spring-Hero-Bank/Helper/ConnectionHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T1908e_Spring_Hero_Bank.Helper
+{
+    public class ConnectionHelper
+    {
+        private const string DatabaseServer = "127.0.0.1";
+        private const string DatabaseName = "";
+        private const string DatabaseUid = "root";
+        private const string DatabasePassword = "";
+        private static MySqlConnection _connection;
+
+        public static MySqlConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                Console.WriteLine("Create new connection...");
+                _connection =
+                    new MySqlConnection(
+                        $"SERVER={DatabaseServer};DATABASE={DatabaseName};UID={DatabaseUid};PASSWORD={DatabasePassword}");
+                Console.WriteLine("...success!");
+            }
+            return _connection;
+        }
+    }
+}
