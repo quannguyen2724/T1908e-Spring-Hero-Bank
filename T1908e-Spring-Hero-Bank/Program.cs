3411184a3a68bbd74fca89c99ed5d8870406dcde
230519 <rrcc@users.noreply.github.com>
2020-06-12 20:22:45 +0700

    test git

--- T1908e-Spring-Hero-Bank/Program.cs
+++ T1908e-Spring-Hero-Bank/Program.cs
@@ -8,6 +8,7 @@ namespace T1908e_Spring_Hero_Bank
         {
             Console.WriteLine("Git Test");
             Console.WriteLine("Git Test 2");
+            Console.WriteLine("Git Test 3");
         }
     }
 }
