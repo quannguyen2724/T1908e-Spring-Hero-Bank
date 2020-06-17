0f9ed5b58ff83d2bc788950386b9831f72b65fb4
Tony <kienttd00782@fpt.edu.vn>
2020-06-17 20:05:32 +0700

    (kien)

--- T1908e-Spring-Hero-Bank/Model/AccountModel.cs
+++ T1908e-Spring-Hero-Bank/Model/AccountModel.cs
@@ -1,4 +1,8 @@
-﻿using MySql.Data.MySqlClient;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+using T1908e_Spring_Hero_Bank.Controller;
 using T1908e_Spring_Hero_Bank.Entity;
 using T1908e_Spring_Hero_Bank.Helper;
 
@@ -7,6 +11,18 @@ namespace T1908e_Spring_Hero_Bank.Model
     public class AccountModel
     {
         private Account _account;
+
+        public void Save(Account _account)
+        {
+            var cnn = ConnectionHelper.GetConnection();
+            cnn.Open();
+            var cmd = new MySqlCommand(
+                $"insert into accounts (accountNumber,balance,username,passwordHash, salt,role,fullname,phone,email,status) values ('{_account.AccountNumber}'," +
+                $"'{_account.Balance}','{_account.Username}','{_account.PasswordHash}','{_account.Salt}','{_account.Role}','{_account.Fullname}','{_account.Phone}'," +
+                $"{_account.Email},'{_account.Status}')", cnn);
+            cmd.ExecuteNonQuery();
+            Console.WriteLine("Success!");
+        }
         public Account GetAccountByUsername(string username)
         {
             var cnn = ConnectionHelper.GetConnection();
@@ -81,6 +97,33 @@ namespace T1908e_Spring_Hero_Bank.Model
             }
             cnn.Close();
             return _account;
+        }
+
+        public List<Account> GetList()
+        {
+            List<Account> list = new List<Account>();
+            var cnn = ConnectionHelper.GetConnection();
+            cnn.Open();
+            var cmd = new MySqlCommand("select * from accounts",cnn);
+            var reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                list.Add(new Account()
+                {
+                    AccountNumber = reader.GetString("accountNumber"),
+                    Balance = reader.GetInt32("balance"),
+                    Username = reader.GetString("username"),
+                    PasswordHash = reader.GetString("passwordHash"),
+                    Email = reader.GetString("email"),
+                    Role = reader.GetInt32("role"),
+                    Fullname = reader.GetString("fullname"),
+                    Phone = reader.GetString("phone")
+
+
+                });
+            }
+            cnn.Close();
+            return list;
         }
     }
 }
