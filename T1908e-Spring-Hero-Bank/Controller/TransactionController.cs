8d854716b3e2366666c5bbf125fc1b94a7cd9f65
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2020-06-18 00:16:50 +0700

    add deposit, withdraw, transfer

--- T1908e-Spring-Hero-Bank/Controller/TransactionController.cs
+++ T1908e-Spring-Hero-Bank/Controller/TransactionController.cs
@@ -1,7 +1,37 @@
-﻿namespace T1908e_Spring_Hero_Bank.Controller
+﻿using System;
+using T1908e_Spring_Hero_Bank.Helper;
+using T1908e_Spring_Hero_Bank.Model;
+
+namespace T1908e_Spring_Hero_Bank.Controller
 {
     public class TransactionController
     {
-        
+        private InputHelper _inputHelper;
+
+        public void Deposit()
+        {
+            var transactionModel = new TransactionModel();
+            Console.WriteLine("Enter the amount: ");
+            var amount = double.Parse(Console.ReadLine());
+            transactionModel.Deposit(AccountController.currentAccount.AccountNumber, amount);
+        }
+
+        public void Withdraw()
+        {
+            var transactionModel = new TransactionModel();
+            Console.WriteLine("Enter the amount: ");
+            var amount = double.Parse(Console.ReadLine());
+            transactionModel.Withdraw(AccountController.currentAccount.AccountNumber, amount);
+        }
+
+        public void Transfer()
+        {
+            var transactionModel = new TransactionModel();
+            Console.WriteLine("Enter receiver account number: ");
+            var receiverAccount = Console.ReadLine();
+            Console.WriteLine("Enter the amount: ");
+            var amount = double.Parse(Console.ReadLine());
+            transactionModel.Transfer(AccountController.currentAccount.AccountNumber, receiverAccount, amount);
+        }
     }
 }
